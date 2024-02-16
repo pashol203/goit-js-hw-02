cd2c490929aa36acf98f49d7fd93620d771a9638
pashol203 <138322995+pashol203@users.noreply.github.com>
2024-02-16 13:54:11 +0300

    ++

--- task-4.js
+++ task-4.js
@@ -1,27 +1,27 @@
-function getShippingCost(country){
-    let price;
-    let message;
-    switch(country){
-        case `China`:
-        price = 100;
-        message = `Shipping to ${country} will cost ${price} credits`;
-        break;
-        case `Chile`:
-            price = 250;
-            message = `Shipping to ${country} will cost ${price} credits`;
-            break;
-            case `Australia`:
-                price = 170;
-                message = `Shipping to ${country} will cost ${price} credits`;
-                break;
-                case `Jamaica`:
-                    price = 120;
-                    message = `Shipping to ${country} will cost ${price} credits`;
-                    break;
-                    default:
-                        message = "Sorry, there is no delivery to your country";
-    }
-    return message;
+function getShippingCost(country) {
+  let price;
+  let message;
+  switch (country) {
+    case `China`:
+      price = 100;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+    case `Chile`:
+      price = 250;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+    case `Australia`:
+      price = 170;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+    case `Jamaica`:
+      price = 120;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+    default:
+      message = "Sorry, there is no delivery to your country";
+  }
+  return message;
 }
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Сорри, не является уникальным для вашей страны"
